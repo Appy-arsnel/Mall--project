fa9a09aeb3daf4ec0173052b37242ab6a3cce3ad
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2022-04-10 18:49:34 +0530

    Commit

--- Assets/Scripts/ToyStore_Manager.cs
+++ Assets/Scripts/ToyStore_Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+ using UnityEngine.SceneManagement;
 
 public class ToyStore_Manager : MonoBehaviour
 {
@@ -47,6 +48,11 @@ public class ToyStore_Manager : MonoBehaviour
 
         PlayerPrefs.SetInt("Selectedtoy",currenttoyindex);
         
+        
+         }
+         public void SceneSelect(){
+     
+          SceneManager.LoadScene("main");
         
          }
 }
--- Assets/Scripts/rotate.cs
+++ Assets/Scripts/rotate.cs
@@ -9,7 +9,8 @@ public class rotate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+          Cursor.visible = true;
+          Time.timeScale = 1; 
     }
 
     // Update is called once per frame
