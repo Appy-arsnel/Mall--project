bce79895c499cc4853b1c9ee3c11b0f17ab266f3
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2022-04-11 12:12:14 +0530

    Commit

---
+++ Assets/Scripts/bookstoreselect.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class bookstoreselect : MonoBehaviour
+{
+    
+         public Material Green;
+   //  public Scene level;
+     public Material Red;
+      public GameObject Object;
+      private bool isinmissionarea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         Object.GetComponent<MeshRenderer> ().material = Red;
+         isinmissionarea=false;
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F) &&isinmissionarea==true )
+            {
+                Object.GetComponent<MeshRenderer> ().material = Green;
+                isinmissionarea =false;  
+            }
+           
+    }
+
+    private void OnTriggerEnter(Collider other)
+
+    {
+       
+       isinmissionarea=true;
+
+          SceneManager.LoadScene("Example_2_Controled Flipping");
+
+    }
+}
