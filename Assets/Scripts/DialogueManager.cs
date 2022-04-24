d30143c2be432f3faa3b35404978c05ca0bf429f
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2022-04-24 16:07:34 +0530

    Commit added

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -4,7 +4,7 @@ using System.Globalization;
 using System.IO.Pipes;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class DialogueManager : MonoBehaviour
 {
     public NPC npc;
@@ -17,77 +17,25 @@ public class DialogueManager : MonoBehaviour
     public GameObject player;
     public GameObject dialogueUI;
 
-    public Text npcName;
-    public Text npcDialogueBox;
-    public Text playerResponse;
+    public TMP_Text npcName;
+    public TMP_Text npcDialogueBox;
+    public TMP_Text playerResponse;
+   private Animator animator;
+     private bool is_waving;
 
-
+     public int i = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         dialogueUI.SetActive(false);
         Cursor.visible = false;
-
+      animator= GetComponent<Animator>();
     }
 
    private void Update() {
        
-        distance = Vector3.Distance(player.transform.position, this.transform.position);
-        if(distance <= 5f)
-        {
-            if(Input.GetAxis("Mouse ScrollWheel") < 0f)
-            {
-                curResponseTracker++;
-                if (curResponseTracker >= npc.playerDialogue.Length - 1)
-                {
-                    curResponseTracker = npc.playerDialogue.Length - 1;
-                }
-            }
-            else if(Input.GetAxis("Mouse ScrollWheel") > 0f)
-            {
-                curResponseTracker--;
-                if(curResponseTracker < 0)
-                {
-                    curResponseTracker = 0;
-                }
-            }
-            //trigger dialogue
-            if(Input.GetKeyDown(KeyCode.E) && isTalking==false)
-            {
-                StartConversation();
-            }
-            else if(Input.GetKeyDown(KeyCode.E) && isTalking == true)
-            {
-                EndDialogue();
-            }
-
-            if(curResponseTracker == 0 && npc.playerDialogue.Length >= 0)
-            {
-                playerResponse.text = npc.playerDialogue[0];
-                if(Input.GetKeyDown(KeyCode.Return))
-                {
-                    npcDialogueBox.text = npc.dialogue[1];
-                }
-            }
-            else if(curResponseTracker == 1 && npc.playerDialogue.Length >= 1)
-            {
-                playerResponse.text = npc.playerDialogue[1];
-                if(Input.GetKeyDown(KeyCode.Return))
-                {
-                    npcDialogueBox.text = npc.dialogue[2];
-                }
-            }
-            else if (curResponseTracker == 2 && npc.playerDialogue.Length >= 2)
-            {
-                playerResponse.text = npc.playerDialogue[2];
-                if (Input.GetKeyDown(KeyCode.Return))
-                {
-                    npcDialogueBox.text = npc.dialogue[3];
-                }
-            }
-
-        }
+        Convo();
     }
 
     void StartConversation()
@@ -97,7 +45,9 @@ public class DialogueManager : MonoBehaviour
         curResponseTracker = 0;
         dialogueUI.SetActive(true);
         npcName.text = npc.name;
-        npcDialogueBox.text = npc.dialogue[0];
+        npcDialogueBox.text = npc.dialogue[i];
+    
+
     }
 
     void EndDialogue()
@@ -105,6 +55,65 @@ public class DialogueManager : MonoBehaviour
         Cursor.visible = false;
         isTalking = false;
         dialogueUI.SetActive(false);
+              i = 0;
     }
+  IEnumerator waving_waiter()
+{     
+     animator.SetBool("iswaving",true);
+    yield return new WaitForSeconds(2);
+   animator.SetBool("iswaving",false);         
 
+}
+void Convo(){
+    distance = Vector3.Distance(player.transform.position, this.transform.position);
+        if(distance <= 5f)
+        {
+            // if(Input.GetAxis("Mouse ScrollWheel") < 0f)
+            // {
+            //     curResponseTracker++;
+            //     if (curResponseTracker >= npc.playerDialogue.Length - 1)
+            //     {
+            //         curResponseTracker = npc.playerDialogue.Length - 1;
+            //     }
+            // }
+            // else if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+            // {
+            //     curResponseTracker--;
+            //     if(curResponseTracker < 0)
+            //     {
+            //         curResponseTracker = 0;
+            //     }
+            // }
+
+
+
+            // change lines of conservations
+
+            
+            //trigger dialogue
+            if(Input.GetKeyDown(KeyCode.E) && isTalking==false)
+            {
+                StartConversation();
+                StartCoroutine(waving_waiter());
+            }
+            else if(Input.GetKeyDown(KeyCode.E) && isTalking == true)
+            {
+                EndDialogue();
+            }
+           
+           
+             if (isTalking == true && Input.GetKeyDown(KeyCode.Space) && i < npc.dialogue.Length - 1)
+        {
+            i++;
+            npcDialogueBox.text = npc.dialogue[i];
+            
+        }
+        else if (isTalking == true && Input.GetKeyDown(KeyCode.Space) && i == npc.dialogue.Length - 1)
+        {
+            EndDialogue();
+        }
+            
+
+        }
+   }
 }
