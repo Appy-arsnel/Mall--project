ada0411adfc1d1cdb936d6a794280a6b9c3f76af
Debashish Majumdar <majumdardebashish@ymail.com>
2022-07-05 02:32:25 +0530

    commit

--- Assets/Scripts/ToyStore_Manager.cs
+++ Assets/Scripts/ToyStore_Manager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ToyStore_Manager : MonoBehaviour
@@ -12,62 +12,79 @@ public class ToyStore_Manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       currenttoyindex=PlayerPrefs.GetInt("Selectedtoy",0);
-        foreach(GameObject toy in toymodels){
+        currenttoyindex = PlayerPrefs.GetInt("Selectedtoy", 0);
+        foreach (GameObject toy in toymodels)
+        {
             toy.SetActive(false);
         }
-        currenttoyindex=0;
+        currenttoyindex = 0;
         toymodels[currenttoyindex].SetActive(true);
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-            Cursor.lockState = CursorLockMode.Confined;
+
+        //  Cursor.lockState = CursorLockMode.Confined;
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneSelect();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ChangPrev();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Changnext();
+        }
 
 
     }
 
-    public void Changnext(){
+    public void Changnext()
+    {
         toymodels[currenttoyindex].SetActive(false);
         currenttoyindex++;
-        if(currenttoyindex==toymodels.Length)   
-        currenttoyindex=0;
+        if (currenttoyindex == toymodels.Length)
+            currenttoyindex = 0;
 
         toymodels[currenttoyindex].SetActive(true);
 
-        PlayerPrefs.SetInt("Selectedtoy",currenttoyindex);
-        
-        
-         }
-         public void ChangPrev(){
+        PlayerPrefs.SetInt("Selectedtoy", currenttoyindex);
+
+
+    }
+    public void ChangPrev()
+    {
         toymodels[currenttoyindex].SetActive(false);
         currenttoyindex--;
-        if(currenttoyindex== -1)   
-        currenttoyindex=toymodels.Length-1;
+        if (currenttoyindex == -1)
+            currenttoyindex = toymodels.Length - 1;
 
         toymodels[currenttoyindex].SetActive(true);
 
-        PlayerPrefs.SetInt("Selectedtoy",currenttoyindex);
-        
-        
-         }
-         public void SceneSelect(){
+        PlayerPrefs.SetInt("Selectedtoy", currenttoyindex);
+
+
+    }
+    public void SceneSelect()
+    {
         DisableButton();
-             Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
         //SceneManager.LoadScene("Example_2_Controled Flipping");
         SceneManager.LoadSceneAsync("main");
-        
-         }
-           public void ToySceneSelect(){
+
+    }
+    public void ToySceneSelect()
+    {
         DisableButton();
-             Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
         DataPersistanceManager.instance.LoadGame();
-          SceneManager.LoadSceneAsync("main");
-        
-         }
+        SceneManager.LoadSceneAsync("main");
+
+    }
 
     void DisableButton()
     {
